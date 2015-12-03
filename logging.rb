fb2923a227d5fb28e35f1b2a93a4b3296899195e
kw90 <kai.waelti@gmail.com>
2015-12-03 16:55:18 +0100

    Added aspect-oriented programming example by means of logging.

---
+++ logging.rb
@@ -0,0 +1,27 @@
+require 'aspector'
+
+# Example class to which we will apply our aspects
+class ExampleClass
+  def test(input)
+    input.upcase
+  end
+end
+
+# Aspect used as a logging hookup
+class LoggingAspect < Aspector::Base
+  ALL_METHODS = /.*/
+
+  around ALL_METHODS, except: :class, method_arg: true do |method, proxy, *args, &block|
+    class_method = "#{self.class}.#{method}"
+    puts "Entering #{class_method}: #{args.join(',')}"
+    result = proxy.call(*args, &block)
+    puts "Exiting  #{class_method}: #{result}"
+    result
+  end
+end
+
+LoggingAspect.apply(ExampleClass)
+puts 'LoggingAspect is applied'
+
+instance = ExampleClass.new
+instance.test 'input'
